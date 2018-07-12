3a1417e3a8114404b55c33a92f468c4241071cdc
Alina Vanieva <vanieva.alina@yandex.ru>
2018-07-12 06:24:53 +0300

    adds board component

---
+++ src/components/board/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import b_ from 'b_';
+import Square from '../square';
+import './index.css';
+
+const b = b_.with('board');
+
+class Board extends Component {
+    renderSquares() {
+        const squares = this.props.squares.reduce((arr, value, i) => {
+            const square = (
+                <Square
+                    key={i}
+                    value={value}
+                    onClick={() => this.props.onClick(i)}
+                />
+            );
+            arr.push(square);
+
+            return arr;
+        }, []);
+
+        return squares;
+    }
+
+    render() {
+        return (
+            <div className={b()}>
+                {this.renderSquares()}
+            </div>
+        );
+    }
+}
+
+export default Board;
